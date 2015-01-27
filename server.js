8a4531c9b3f3cfa0283a632afb90dc1dbeeced2d
MatthewHilty <hilty.matthew@gmail.com>
2015-01-26 23:38:05 -0500

    Ping heroku repeatedly to prevent spin-down.

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ var cookieParser = require('cookie-parser');
 var defaultState = require('./lib/default-state');
 var express      = require('express');
 var fs           = require('fs');
+var http         = require('http');
 var isObject     = require('./lib/utilities').isObject;
 var path         = require('path');
 var React        = require('./lib/vendor/React');
 var TodoApp      = require('./lib/view/app');
 
-var anchor   = '$TODOAPP$';
-var app      = express();
-var port     = process.env.PORT || 4000;
-var utf8     = { encoding: 'utf8' };
-var template = fs.readFileSync('./index.html', utf8);
+var anchor    = '$TODOAPP$';
+var app       = express();
+var herokuApp = 'http://todomvc-reactive-aspen.herokuapp.com'
+var port      = process.env.PORT || 4000;
+var utf8      = { encoding: 'utf8' };
+var template  = fs.readFileSync('./index.html', utf8);
 
 function getAppState(req) {
   var cookie = req.cookies.aspenTodoAppState;
@@ -20,6 +22,17 @@ function getAppState(req) {
 
 function onStart() {
   console.log('Server running on port ' + port);
+  preventHerokuSleep();
+  console.log('Pinging app to prevent spin-down.');
+}
+
+function pingHeroku() {
+  http.get(herokuApp);
+}
+
+function preventHerokuSleep() {
+  var fiveMin = 5 * 60 * 1000;
+  setInterval(pingHeroku, fiveMin);
 }
 
 function renderReactToHtml(req, res) {
