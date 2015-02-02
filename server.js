a46c534742a23d055e5230096775c1737afb4395
MatthewHilty <hilty.matthew@gmail.com>
2015-02-02 16:44:31 -0500

    Have route determine the mode of the markup; add app-state to the markup so that the client can initialize itself; remove unnecessary imports; add paths for 'active' and 'completed' modes; add a default path.

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
+'use strict';
+
 var cookieParser = require('cookie-parser');
-var defaultState = require('./lib/default-state');
 var express      = require('express');
 var fs           = require('fs');
 var http         = require('http');
-var isObject     = require('./lib/utilities').isObject;
 var path         = require('path');
 var React        = require('./lib/vendor/React');
 var TodoApp      = require('./lib/view/app');
 
-var anchor    = '$TODOAPP$';
-var app       = express();
-var herokuApp = 'http://todomvc-reactive-aspen.herokuapp.com'
-var port      = process.env.PORT || 4000;
-var utf8      = { encoding: 'utf8' };
-var template  = fs.readFileSync('./index.html', utf8);
+var app          = express();
+var appAnchor    = '$TODOAPP$';
+var defaultState = '{"editing":null,"mode":"all","todos":[]}';
+var herokuApp    = 'http://todomvc-reactive-aspen.herokuapp.com';
+var port         = process.env.PORT || 4000;
+var stateAnchor  = '$APPSTATE$';
+var utf8         = { encoding: 'utf8' };
+var template     = fs.readFileSync('./templates/app', utf8);
 
-function getAppState(req) {
+function getAppStateStr(mode, req) {
   var cookie = req.cookies.aspenTodoAppState;
-  return cookie ? JSON.parse(cookie) : defaultState;
+  var state  = cookie ? cookie : defaultState;
+  return resetMode(mode, state);
 }
 
 function onStart() {
   console.log('Server running on port ' + port + '.');
   preventHerokuSleep();
-  console.log('Pinging app to prevent spin-down.');
 }
 
 function pingHeroku() {
@@ -33,19 +35,31 @@ function pingHeroku() {
 function preventHerokuSleep() {
   var fiveMin = 5 * 60 * 1000;
   setInterval(pingHeroku, fiveMin);
+  console.log('Pinging app to prevent spin-down.');
 }
 
-function renderReactToHtml(req, res) {
-  var appState = getAppState(req);
-  var markup = React.renderToString(TodoApp(appState));
-  res.send(template.replace(anchor, markup));
+function renderReactToHtml(mode) {
+  return function (req, res) {
+    var appStateStr = getAppStateStr(mode, req);
+    var appState    = JSON.parse(appStateStr);
+    var appMarkup   = React.renderToString(TodoApp(appState));
+
+    var html = template.replace(appAnchor,   appMarkup)
+                         .replace(stateAnchor, appStateStr);
+    res.send(html);
+  };
+}
+
+function resetMode(mode, state) {
+  return state.replace(/"mode":".*?"/, ('"mode":"' + mode + '"'));
 }
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cookieParser());
 
-app.get('/', renderReactToHtml);
-app.get('/index.html', renderReactToHtml);
+app.get('/active',       renderReactToHtml('active'));
+app.get('/completed',    renderReactToHtml('completed'));
+app.get('*',             renderReactToHtml('all'));
 
 app.listen(port, onStart);
